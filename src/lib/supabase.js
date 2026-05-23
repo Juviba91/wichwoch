6cd8c3f5992a24001f1d9770f0970fb33782f937
Juviba91 <102589663+Juviba91@users.noreply.github.com>
2026-05-23 10:37:16 +0200

    Initialize Supabase client in supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+export const supabase = createClient(
+  "https://kmxpachollvsiytppvyy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtteHBhY2hvbGx2c2l5dHBwdnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NTk0MTYsImV4cCI6MjA5NDIzNTQxNn0.w6tVaItGQi-tuWuoqRqcRl6Z7gIpBIFIcji6szRTXI4"
+);
